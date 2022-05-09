8d536f961fefefc1e9760b963d1eb6fb133efe2a
anigandini <anigandini@gmail.com>
2022-05-09 19:07:51 -0300

    first commit

---
+++ src/components/Intro.tsx
@@ -0,0 +1,13 @@
+const Intro = () => {
+    return <>
+        <figure className='cookie strawberry-heart'></figure>
+        <h1>Cookie</h1>
+        <label>
+        <input type="radio" className="nes-radio is-dark" name="start" checked />
+        <span>Start</span>
+        </label>
+        <a href='https://github.com/anigandini' target='_blank'><h2>By Ani Gandini</h2></a>
+    </>
+}
+
+export default Intro
